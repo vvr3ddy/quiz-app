cf58732c80873e8df2d252583cdb5b405a2868fa
vvr3ddy <vvr-rockstar@bmsit.in>
2020-08-03 11:07:47 +0530

    :sparkles: Initial Import
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

---
+++ src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+    <b-nav>
+    <b-nav-item disabled><b>Quiz App</b></b-nav-item>
+    <b-nav-item disabled>Counter: 4/10</b-nav-item>
+  </b-nav>
+</template>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <b-jumbotron>
+      <template v-slot:lead>Sample Question</template>
+
+      <hr class="my-4" />
+
+      <div>
+        <b-form-group label="Sample Options">
+          <b-form-radio v-model="selected" name="some-radios" value="A">Option A</b-form-radio>
+          <b-form-radio v-model="selected" name="some-radios" value="B">Option B</b-form-radio>
+        </b-form-group>
+
+        <div class="mt-3">
+          Your Choice:
+          <strong>{{ selected }}</strong>
+        </div>
+      </div>
+      <b-container class="bv-example-row">
+        <b-row>
+          <b-col sm="2">
+            <b-button variant="danger" href="#">Clear Choices</b-button>
+          </b-col>
+          <b-col sm="2" offset="2">
+            <b-button variant="success" href="#">Submit Test</b-button>
+          </b-col>
+          <b-col sm="2" offset="2">
+            <b-button variant="primary" href="#">Next Question</b-button>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selected: "",
+    };
+  },
+};
+</script>
